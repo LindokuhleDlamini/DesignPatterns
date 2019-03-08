7a310430b6dcf601578cf5f3a37bd37518a46d26
Lindokuhle Dlamini <DLMLIN017@myuct.ac.za>
2019-03-08 13:12:03 +0200

    implemented the mediator design pattern

---
+++ DesignPatterns/MediatorPattern/AirTrafficControlMediator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.MediatorPattern
+{
+    public class AirTrafficControlMediator : IAirTrafficControlMediator
+    {
+        private Flight _flight;
+        private Runway _runway;
+        private bool _runwayStatus;
+
+        public bool IsRunwayAvailable()
+        {
+            return _runwayStatus;
+        }
+
+        public void SetFlight(Flight flight)
+        {
+            _flight = flight;
+        }
+
+        public void SetRunwayStatus(bool runwayStatus)
+        {
+            _runwayStatus = runwayStatus;
+        }
+
+        public void SetRunway(Runway runway)
+        {
+            _runway = runway;
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesignPatterns.MediatorPattern;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,20 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
+            MediatorPatternClient();
+            Console.ReadLine();
+        }
+
+        public static void MediatorPatternClient()
+        {
+            var aTCMediator = new AirTrafficControlMediator();
+            var flight93 = new Flight(aTCMediator);
+            var runway = new Runway(aTCMediator);
+            aTCMediator.SetFlight(flight93);
+            aTCMediator.SetRunway(runway);
+            flight93.FlightReadyToLand();
+            runway.Land();
+            flight93.Land();
         }
     }
 }
